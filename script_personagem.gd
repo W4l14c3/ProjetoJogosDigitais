0040a88d4dfa5e94fb91bfa86ad9bdb04b8d043f
W4l14c3 <wallacedias2016@gmail.com>
2024-11-02 21:45:36 -0300

    Mapa atualizado e movimento adicionado

--- script_personagem.gd
+++ script_personagem.gd
@@ -6,5 +6,24 @@ var forca_pulo = 100
 var mov = Vector2.ZERO
 
 func _process(delta):
-	pass
+	movimento()
+	
+#FUNÇÕES GENERICAS ABAIXO:
+func movimento():
+	#Função para movimento e queda...
+	mov.x = 0
+	mov.y += gravidade
+	
+	while(mov.x == 0):
+		$AnimationPlayer.play("idle")
+	if(Input.action_press("ui_right")):
+		mov.x = -velocidade
+		$Sprite.flip_h = false
+		$AnimationPlayer.play("Run")
+	elif(Input.action_press("ui_left")):
+		mov.x = +velocidade
+		$Sprite.flip_h = true
+		$AnimationPlayer.play("Run")
+	else:
+		$AnimationPlayer.play("idle")
 	
