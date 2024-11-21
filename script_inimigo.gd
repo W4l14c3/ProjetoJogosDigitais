ef72c520be214e449960c5d170363986b5a6c866
W4l14c3 <wallacedias2016@gmail.com>
2024-11-21 00:51:36 -0300

    Movimento do inimigo aprimorado, Teste de zona de detecção...

--- script_inimigo.gd
+++ script_inimigo.gd
@@ -8,27 +8,56 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start()
 	pass # Replace with function body.
+	
 
-var velocidade = 100
+var velocidade = 50
 var direcao = 1
 var forca_gravidade = 30
 var mov = Vector2.ZERO
+var movAleAtivo = true
 
 func _process(delta):
-	mov.y += forca_gravidade
-	if(not $RayCast2D2Direito.is_colliding()):
-		direcao = -1
-		$AnimatedSprite.flip_h = true
-		
-	if(not $RayCast2D2Esquerdo.is_colliding()):
-		direcao = 1
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.animation("")
-		
-	if (direcao == 1):
-		mov.x = velocidade
-	elif(direcao == -1):
-		mov.x = -velocidade
+	movimentoAle()
 	
-	mov = move_and_slide(mov)
+func movimentoAle():
+	if(movAleAtivo):
+		mov.y += forca_gravidade
+		if(round($Timer.time_left) < 7):
+			$AnimatedSprite.play("Idle")
+			mov.x = 0
+			
+		elif(round($Timer.time_left) > 7):
+			$AnimatedSprite.play("Run")
+			if(not $RayCast2D2Direito.is_colliding()):
+				direcao = -1
+				$AnimatedSprite.flip_h = true
+				
+				
+			if(not $RayCast2D2Esquerdo.is_colliding()):
+				direcao = 1
+				$AnimatedSprite.flip_h = false
+
+			if (direcao == 1):
+				mov.x = velocidade
+			elif(direcao == -1):
+				mov.x = -velocidade
+			
+			mov = move_and_slide(mov)
+
+
+
+
+func ZonaDeDeteccaoDireita(body):
+#	movAleAtivo = false
+#	mov.x = -velocidade
+#	mov = move_and_slide(mov)
+	pass
+#
+
+func ZonaDeDeteccaoEsquerda(body):
+#	movAleAtivo = false
+#	mov.x = velocidade
+#	mov = move_and_slide(mov)
+	pass
